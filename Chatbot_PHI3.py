cdc0953741f38e7d2d03916222006a39e269d19f
Aditya Sharma <114298399+Adi1116@users.noreply.github.com>
2024-05-04 11:07:11 +0530

    Update Chatbot_PHI3.py

--- Chatbot_PHI3.py
+++ Chatbot_PHI3.py
@@ -14,34 +14,34 @@
 #import streamlit as st
 #import ollama
 
-st.title("Phi3 Chatbot (by Aditya Sharma)")
+streamlit.title("Phi3 Chatbot (by Aditya Sharma)")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Hello there, how can I assist you today?"}]
+if "messages" not in streamlit.session_state:
+    streamlit.session_state["messages"] = [{"role": "assistant", "content": "Hello there, how can I assist you today?"}]
 
-if st.session_state.get("messages"):
-    for msg in st.session_state.get("messages"):
+if streamlit.session_state.get("messages"):
+    for msg in streamlit.session_state.get("messages"):
         if msg["role"] == "user":
-            st.chat_message(msg["role"]).write(msg["content"])
+            streamlit.chat_message(msg["role"]).write(msg["content"])
         else:
-            st.chat_message(msg["role"]).write(msg["content"])
+            streamlit.chat_message(msg["role"]).write(msg["content"])
 
 ## Configure the model
 def generate_response():
-    if "full_message" not in st.session_state:
-        st.session_state["full_message"] = ""
+    if "full_message" not in streamlit.session_state:
+        streamlit.session_state["full_message"] = ""
         
-    response = ollama.chat(model='phi3', stream=True, messages=st.session_state["messages"])
+    response = ollama.chat(model='phi3', stream=True, messages=streamlit.session_state["messages"])
     for partial_resp in response:
         token = partial_resp["message"]["content"]
-        st.session_state["full_message"] += token
+        streamlit.session_state["full_message"] += token
         yield token
 
-if prompt := st.chat_input():
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = []
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    st.session_state["full_message"] = ""
-    st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
-    st.session_state["messages"].append({"role": "assistant", "content": st.session_state["full_message"]})
+if prompt := streamlit.chat_input():
+    if "messages" not in streamlit.session_state:
+        streamlit.session_state["messages"] = []
+    streamlit.session_state["messages"].append({"role": "user", "content": prompt})
+    streamlit.chat_message("user").write(prompt)
+    streamlit.session_state["full_message"] = ""
+    streamlit.chat_message("assistant", avatar="🤖").write_stream(generate_response)
+    streamlit.session_state["messages"].append({"role": "assistant", "content": streamlit.session_state["full_message"]})
